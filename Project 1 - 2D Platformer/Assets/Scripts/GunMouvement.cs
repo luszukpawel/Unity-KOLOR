21d76110e712b3ca748e33b4e4de083b61fd6f23
pluszak <luszukpawel@gmail.com>
2017-05-12 11:41:22 +0200

    lesson 1

---
+++ Project 1 - 2D Platformer/Assets/Scripts/GunMouvement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMouvement : MonoBehaviour
+{
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 gunPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - gunPos.x;
+        mousePos.y = mousePos.y - gunPos.y;
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
